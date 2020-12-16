1ae525cae3e7dac4e937cb2c9389cea81e708378
nagaokayuji <nagaokayuji@team-lab.com>
2020-12-16 13:34:05 +0900

    plot practice

--- mle.py
+++ mle.py
@@ -1,10 +1,23 @@
 # %%
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 a = np.linspace(0, 10, 100)
 b = np.exp(-a)
 plt.plot(a, b)
+plt.show()
+
+# %%
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm)
 plt.show()
 
 # %%
