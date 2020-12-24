47198fd3dcf9928d33ccbda25c3ec1990ef55019
nagaokayuji <nagaokayuji@team-lab.com>
2020-12-24 18:13:37 +0900

    test markdown

--- 正規分布.py
+++ 正規分布.py
@@ -11,12 +11,17 @@ x = np.linspace(-100, 100, 300)
 sigma = 20
 mu = 0
 # %%
-x
 # %%
 y = 1. / ((2*np.pi)**0.5 * sigma) * np.exp(- ((x - mu)**2) / (2 * sigma**2))
 
-# %%
-
 plt.plot(x, y)
 # %%
+# %% [markdown]
+# # 乱数を生成
+x = np.random.normal(50, 10, 1000)
+# %% [markdown]
+# # ヒストグラムを出力
+# %%
+plt.hist(x, bins=20)
+
 # %%
