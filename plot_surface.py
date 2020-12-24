8225ad4afe22c99e1e5ac21a937ae2a0f8a9a9d6
Yuji Nagaoka <yvay5cqe@gmail.com>
2020-12-25 01:02:46 +0900

    clean

--- plot_surface.py
+++ plot_surface.py
@@ -4,10 +4,6 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-a = np.linspace(0, 10, 100)
-b = np.exp(-a)
-plt.plot(a, b)
-plt.show()
 
 # %%
 fig = plt.figure()
