ae33b19af1e4a7736517e224af928a6aa87aa819
Yuji Nagaoka <yvay5cqe@gmail.com>
2020-12-25 03:37:37 +0900

    add scatter_matrix

--- sample_iris.py
+++ sample_iris.py
@@ -1,13 +1,17 @@
 # %%
+from sklearn.model_selection import train_test_split
 import pandas as pd
+import mglearn
 from sklearn.datasets import load_iris
-
+# %%
 data = load_iris()
 X = pd.DataFrame(data.data, columns=data.feature_names)
 y = pd.DataFrame(data.target, columns=["Species"])
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=77)
 # %%
-df = pd.concat([X, y], axis=1)
-# %%
-df.head()
 
+grr = pd.plotting.scatter_matrix(X, figsize=(15, 15), marker='x',
+                                 hist_kwds={'bins': 20}, s=60)
+
+# %%
 # %%
